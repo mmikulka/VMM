8dfb4e386cd1fb4bf67b1d9d52882d0e936f1597
Aedan Valle <avhslover@csu.fullerton.edu>
2019-05-08 20:12:32 -0700

    Add files via upload

---
+++ LRU.cpp
@@ -0,0 +1,17 @@
+//
+//  LRU.cpp
+//  crap
+//
+//  Created by OrderInChaos on 5/8/19.
+//  Copyright © 2019 OrderInChaos. All rights reserved.
+//
+
+#include "LRU.hpp"
+
+LRU::LRU() {
+    
+}
+
+LRU::~LRU(){
+    
+}
---
+++ LRU.hpp
@@ -0,0 +1,24 @@
+//
+//  LRU.hpp
+//  crap
+//
+//  Created by OrderInChaos on 5/8/19.
+//  Copyright © 2019 OrderInChaos. All rights reserved.
+//
+
+#ifndef LRU_hpp
+#define LRU_hpp
+
+#include <stdio.h>
+#include "PRA.hpp"
+
+class LRU : public PRA {
+public:
+    LRU();
+    ~LRU();
+    
+private:
+    
+};
+
+#endif /* LRU_hpp */
